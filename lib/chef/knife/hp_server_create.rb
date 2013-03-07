b23a04a1287f174ba76c95801c94f79f018df970
Matt Ray <matthewhray@gmail.com>
2013-03-07 12:17:30 -0600

    show the security groups

--- lib/chef/knife/hp_server_create.rb
+++ lib/chef/knife/hp_server_create.rb
@@ -218,7 +218,7 @@ class Chef
       msg_pair("Instance Name", server.name)
       msg_pair("Flavor", server.flavor['id'])
       msg_pair("Image", server.image['id'])
-      #msg_pair("Security Group(s)", server.security_groups.join(", "))
+      msg_pair("Security Group(s)", server.security_groups.join(", "))
       msg_pair("SSH Key Pair", server.key_name)
       msg_pair("Public IP Address", server.public_ip_address)
       msg_pair("Private IP Address", server.private_ip_address)
